6fb40d4b734438bd2fdfc1ac788a95b4eaeb141f
Thomas Michels Rodrigues <thomasmichels.bnu@gmail.com>
2022-07-15 22:00:25 -0300

    feat: Added Users model and repo

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from src import UserRepo
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello_word():
     return "Ola"
+
+
+@app.route("/insert", methods=["POST"])
+def insert():
+    
+    userRepo = UserRepo()
+    body = request.json
+
+    userRepo.insert_user(body["name"])
+    return 'OK'
